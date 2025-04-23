2b8f77c43f0ca918533b02e2a112f49c3bcbd96a
lima73777@gmail.com <lima73777@gmail.com>
2025-04-23 15:13:20 -0300

    fix

--- Vista/Program.cs
+++ Vista/Program.cs
@@ -16,6 +16,7 @@ namespace Vista
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+            //Application.Run(new FrmLogin());
             //Application.Run(new Loading());
         }
     }
